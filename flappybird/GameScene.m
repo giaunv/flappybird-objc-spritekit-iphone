62b48101c43f0264882251f3dde09a69e5b63ecb
Giau Ngo <giaunv@gmail.com>
2015-03-19 02:33:52 +0700

    Animating the bird

--- flappybird/GameScene.m
+++ flappybird/GameScene.m
@@ -19,10 +19,16 @@
     if (self = [super initWithSize:size]) {
         SKTexture *birdTexture1 = [SKTexture textureWithImageNamed:@"Bird1"];
         birdTexture1.filteringMode = SKTextureFilteringNearest;
+        SKTexture *birdTexture2 = [SKTexture textureWithImageNamed:@"Bird2"];
+        birdTexture2.filteringMode = SKTextureFilteringNearest;
+        
+        SKAction *flap = [SKAction repeatActionForever:[SKAction animateWithTextures:@[birdTexture1, birdTexture2] timePerFrame:0.2]];
         
         _bird = [SKSpriteNode spriteNodeWithTexture:birdTexture1];
         [_bird setScale:2.0];
         _bird.position = CGPointMake(self.frame.size.width/4, CGRectGetMidY(self.frame));
+        [_bird runAction:flap];
+        
         [self addChild:_bird];
     }
     
