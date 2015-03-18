9714e48343c3c5c57466216c34d7f510dd8d8d83
Giau Ngo <giaunv@gmail.com>
2015-03-19 02:48:14 +0700

    Adding Ground and Background

--- flappybird/GameScene.m
+++ flappybird/GameScene.m
@@ -10,6 +10,7 @@
 
 @interface GameScene(){
     SKSpriteNode * _bird;
+    SKColor *_skyColor;
 }
 @end
 
@@ -17,6 +18,10 @@
 
 -(id)initWithSize:(CGSize)size{
     if (self = [super initWithSize:size]) {
+        _skyColor = [SKColor colorWithRed:113.0/255.0 green:197.0/255.0 blue:207.0/255.0 alpha:1.0];
+        [self setBackgroundColor:_skyColor];
+        
+        // Adding bird
         SKTexture *birdTexture1 = [SKTexture textureWithImageNamed:@"Bird1"];
         birdTexture1.filteringMode = SKTextureFilteringNearest;
         SKTexture *birdTexture2 = [SKTexture textureWithImageNamed:@"Bird2"];
@@ -30,6 +35,29 @@
         [_bird runAction:flap];
         
         [self addChild:_bird];
+        
+        // Adding ground
+        SKTexture *groundTexture = [SKTexture textureWithImageNamed:@"Ground"];
+        groundTexture.filteringMode = SKTextureFilteringNearest;
+        
+        for (int i = 0; i < 2+self.frame.size.width/(groundTexture.size.width*2); ++i) {
+            SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:groundTexture];
+            [sprite setScale:2.0];
+            sprite.position = CGPointMake(i*sprite.size.width, sprite.size.height/2);
+            [self addChild:sprite];
+        }
+        
+        // Create skyline
+        SKTexture *skylineTexture = [SKTexture textureWithImageNamed:@"Skyline"];
+        skylineTexture.filteringMode = SKTextureFilteringNearest;
+        
+        for (int i = 0; i < 2+self.frame.size.width/(skylineTexture.size.width*2); ++i) {
+            SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:skylineTexture];
+            [sprite setScale:2.0];
+            sprite.zPosition = -20;
+            sprite.position = CGPointMake(i*sprite.size.width, sprite.size.height/2 + groundTexture.size.height*2);
+            [self addChild:sprite];
+        }
     }
     
     return self;
