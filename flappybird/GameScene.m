70a35892de524a10fa17b8dea7e1f95e993c5acd
Giau Ngo <giaunv@gmail.com>
2015-03-19 03:01:13 +0700

    Moving Ground and Background

--- flappybird/GameScene.m
+++ flappybird/GameScene.m
@@ -40,10 +40,15 @@
         SKTexture *groundTexture = [SKTexture textureWithImageNamed:@"Ground"];
         groundTexture.filteringMode = SKTextureFilteringNearest;
         
+        SKAction *moveGroundSprite = [SKAction moveByX:-groundTexture.size.width*2 y:0 duration:0.02*groundTexture.size.width*2];
+        SKAction *resetGroudSprite = [SKAction moveByX:groundTexture.size.width*2 y:0 duration:0];
+        SKAction *moveGroundSpriteForever = [SKAction repeatActionForever:[SKAction sequence:@[moveGroundSprite, resetGroudSprite]]];
+        
         for (int i = 0; i < 2+self.frame.size.width/(groundTexture.size.width*2); ++i) {
             SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:groundTexture];
             [sprite setScale:2.0];
             sprite.position = CGPointMake(i*sprite.size.width, sprite.size.height/2);
+            [sprite runAction:moveGroundSpriteForever];
             [self addChild:sprite];
         }
         
@@ -51,11 +56,16 @@
         SKTexture *skylineTexture = [SKTexture textureWithImageNamed:@"Skyline"];
         skylineTexture.filteringMode = SKTextureFilteringNearest;
         
+        SKAction *moveSkylineSprite = [SKAction moveByX:-skylineTexture.size.width*2 y:0 duration:0.1 * skylineTexture.size.width*2];
+        SKAction *resetSkylineSprite = [SKAction moveByX:skylineTexture.size.width*2 y:0 duration:0];
+        SKAction *moveSkylineSpriteForever = [SKAction repeatActionForever:[SKAction sequence:@[moveSkylineSprite, resetSkylineSprite]]];
+        
         for (int i = 0; i < 2+self.frame.size.width/(skylineTexture.size.width*2); ++i) {
             SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithTexture:skylineTexture];
             [sprite setScale:2.0];
             sprite.zPosition = -20;
             sprite.position = CGPointMake(i*sprite.size.width, sprite.size.height/2 + groundTexture.size.height*2);
+            [sprite runAction:moveSkylineSpriteForever];
             [self addChild:sprite];
         }
     }
